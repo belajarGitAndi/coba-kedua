2a8248826f7b5ae6196064a4e1ed5a23602fe049
andiks2018 <andiks2018@gmail.com>
2021-06-08 18:03:33 +0700

    selesai

---
+++ konstruksi-dasar.py
@@ -0,0 +1,15 @@
+print ("hello World")
+if __name__ == "__main__":
+    print("hello")
+
+#materi percabangan
+# percabangan
+ingin_cepat = False
+if ingin_cepat:
+    print('jalan lurus aja ya ')
+else:
+    print ('jalan lain')
+jumlah_anak = 4
+
+for index_anak in range (1, jumlah_anak+1):
+    print (f'Halo anak {index_anak}')
---
+++ tipeData.py
@@ -0,0 +1,18 @@
+print ("tipe data dictionary ")
+"""
+tipe data dikctionari se=kedar menghuibungkan antara key dan value 
+"""
+kamus = {}
+kamus['anak']= 'son'
+kamus['istri']= 'wife'
+kamus['ayah']= 'father'
+kamus['paman']= 'uncle'
+kamus['aunty']= 'tante'
+print(kamus['ayah'])
+print ('data ini dikirimkan ke driver gojek')
+data_dari_server_gojek={
+    'tanggal'='2020-06-10'
+              
+    
+    ''
+}
